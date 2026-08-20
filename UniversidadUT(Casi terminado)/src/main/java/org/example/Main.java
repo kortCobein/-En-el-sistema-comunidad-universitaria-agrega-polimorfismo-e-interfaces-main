e89e31de63596271e1108c57fb6a42b7ac8df983
kortCobein <kortcobein@iokort.com>
2026-08-20 10:13:59 -0600

    Agregar código fuente

---
+++ UniversidadUT(Casi terminado)/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.vista.MenuGeneral;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        MenuGeneral.menu();
+
+    }
+}
